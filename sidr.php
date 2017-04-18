b366f7bef2976509c6c1181ee34e74b81001974b
anoddnumber <suchanoddnumber@gmail.com>
2017-04-17 22:02:37 -0700

    Remove sidr-left and sidr-right div generation

--- sidr.php
+++ sidr.php
@@ -10,21 +10,21 @@
  */
 
 /*
- *	Add the necessary styles and javascript files
+ *  Add the necessary styles and javascript files
  */
 function sidr_scripts_styles(){
 
-	/*
-	 * Adds JavaScript for handling the navigation menu hide-and-show behavior.
-	 */
-	wp_enqueue_script( 'sidr' , plugins_url('js/jquery.sidr.min.js', __FILE__ ), array('jquery'), null, false );
-	wp_enqueue_script( 'sidrinit' , plugins_url('js/sidr-init.js', __FILE__ ), array('sidr'), null, true );
+  /*
+   * Adds JavaScript for handling the navigation menu hide-and-show behavior.
+   */
+  wp_enqueue_script( 'sidr' , plugins_url('js/jquery.sidr.min.js', __FILE__ ), array('jquery'), null, false );
+  wp_enqueue_script( 'sidrinit' , plugins_url('js/sidr-init.js', __FILE__ ), array('sidr'), null, true );
 
 
-	/*
-	 * Loads stylesheet.
-	 */
-	wp_enqueue_style( 'sidr-style', plugins_url('stylesheets/jquery.sidr.dark.css', __FILE__ ) );
+  /*
+   * Loads stylesheet.
+   */
+  wp_enqueue_style( 'sidr-style', plugins_url('stylesheets/jquery.sidr.dark.css', __FILE__ ) );
 
 }
 
@@ -33,26 +33,26 @@ function sidr_scripts_styles(){
  */
 function sidr_footer() {
 
-	if ( is_active_sidebar( 'sidr-left' ) ) :
-		echo ' 	<div id="sidr-left" class="sidebar-container sidr" role="complementary">
-					<div class="sidr-toggle"><a class="sidr-left-link" href="#sidr-left">Close</a></div>
-					<div class="widget-area">';
-						dynamic_sidebar( 'sidr-left' );
-		echo ' 		</div><!-- .widget-area -->
-				</div><!-- #sidr-left -->';
+  if ( is_active_sidebar( 'sidr-left' ) ) :
+    echo '   <div id="sidr-left" class="sidebar-container sidr" role="complementary">
+          <div class="sidr-toggle"><a class="sidr-left-link" href="#sidr-left">Close</a></div>
+          <div class="widget-area">';
+            dynamic_sidebar( 'sidr-left' );
+    echo '     </div><!-- .widget-area -->
+        </div><!-- #sidr-left -->';
 
-	endif;
+  endif;
 
-	if ( is_active_sidebar( 'sidr-right' ) ) :
-		echo ' 	<div id="sidr-right" class="sidebar-container sidr" role="complementary">
-					<div class="sidr-toggle"><a class="sidr-right-link" href="#sidr-right">Close</a></div>
-					<div class="widget-area">';
-						dynamic_sidebar( 'sidr-right' );
-		echo ' 		</div><!-- .widget-area -->
-				</div><!-- #sidr-right -->';
-				
-	endif;
-				
+  if ( is_active_sidebar( 'sidr-right' ) ) :
+    echo '   <div id="sidr-right" class="sidebar-container sidr" role="complementary">
+          <div class="sidr-toggle"><a class="sidr-right-link" href="#sidr-right">Close</a></div>
+          <div class="widget-area">';
+            dynamic_sidebar( 'sidr-right' );
+    echo '     </div><!-- .widget-area -->
+        </div><!-- #sidr-right -->';
+        
+  endif;
+        
 }
 
 /*
@@ -60,8 +60,8 @@ function sidr_footer() {
  */
 function sidr_setup() {
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menu( 'sidr-menu', __( 'Sidr Menu' ) );
+  // This theme uses wp_nav_menu() in one location.
+  register_nav_menu( 'sidr-menu', __( 'Sidr Menu' ) );
 
 }
 
@@ -70,30 +70,29 @@ function sidr_setup() {
  */ 
 function sidr_widgets_init() {
 
-	register_sidebar( array(
-		'name' => __( 'Sidr Left Sidebar' ),
-		'id' => 'sidr-left',
-		'description' => __( 'Left-hand Side Menu' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>',
-	) );
+  register_sidebar( array(
+    'name' => __( 'Sidr Left Sidebar' ),
+    'id' => 'sidr-left',
+    'description' => __( 'Left-hand Side Menu' ),
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+  ) );
 
-	register_sidebar( array(
-		'name' => __( 'Sidr Right Sidebar' ),
-		'id' => 'sidr-right',
-		'description' => __( 'Right-hand Side Menu' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>',
-	) );
+  register_sidebar( array(
+    'name' => __( 'Sidr Right Sidebar' ),
+    'id' => 'sidr-right',
+    'description' => __( 'Right-hand Side Menu' ),
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+  ) );
 
 }
 
 add_action( 'wp_enqueue_scripts', 'sidr_scripts_styles' );
-add_action( 'wp_footer' , 'sidr_footer' );
 add_action( 'after_setup_theme', 'sidr_setup' );
 add_action( 'widgets_init' , 'sidr_widgets_init' );
 ?>
